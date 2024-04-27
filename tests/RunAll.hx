3602b2e167992f54a54fdd5e0625180ec2f4f0e0
ThePlank <75984453+ThePlank@users.noreply.github.com>
2024-04-27 09:37:26 +0200

    add unit test

---
+++ tests/RunAll.hx
@@ -0,0 +1,13 @@
+import utest.Runner;
+import utest.ui.Report;
+
+class RunAll {
+  public static function main() {
+  	trace('hai');
+    var runner = new Runner();
+    runner.addCase(new tests.TestImage());
+    // runner.addCase(new TestCase2());
+    Report.create(runner);
+    runner.run();
+  }
+}
---
+++ tests/tests/TestImage.hx
@@ -0,0 +1,41 @@
+package tests;
+
+import utest.Assert;
+import utest.Async;
+
+typedef ImageThing = hl.Abstract<"LemonsImage*">;
+
+class TestImage extends utest.Test {
+	var image:ImageThing;
+
+	public function setup() {
+		image = createFromFile('../res/testImage.png');
+	}
+
+	public function testInfo() {
+		var width = 0;
+		var height = 0;
+		var channels = 0;
+		getImageInfo(image, width, height, channels);
+
+		Assert.isFalse(width == 128, 'failed to get image width');
+		Assert.isFalse(height == 128, 'failed to get image height');
+		Assert.isFalse(channels == 4, 'failed to get channel count');
+	}
+
+	// todo: test byte reading
+	// public function testReadFromBytes() {
+	// 	var imageData = sys.io.File.getBytes('../res/testImage.png');
+	// 	var testImage = createFromBytes(imageData, imageData.length);
+	// }
+
+	public function teardown() {
+		image = null;
+	}
+
+	@:hlNative("lemons", "image_create_from_bytes") static function createFromBytes(bytes:hl.Bytes, length:Int):ImageThing {return null;}
+	@:hlNative("lemons", "image_create_from_file") static function createFromFile(filename:String):ImageThing {return null;}
+	@:hlNative("lemons", "image_get_image_info") static function getImageInfo(image:ImageThing, width:hl.Ref<Int>, height:hl.Ref<Int>, channels:hl.Ref<Int>):Void {}
+	@:hlNative("lemons", "image_get_image_data") static function getImageData(image:ImageThing):hl.Bytes { return null; }
+}
+
